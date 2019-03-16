0aab54f9449f424599f4c0a4b6177b70ad2eec2f
Danilo Leite <danilo@moobie.com.br>
2019-03-16 15:32:31 -0300

    first interaction

--- app/src/main/java/br/com/caelum/twittelumapp/TweetActivity.kt
+++ app/src/main/java/br/com/caelum/twittelumapp/TweetActivity.kt
@@ -1,12 +1,28 @@
 package br.com.caelum.twittelumapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class TweetActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botao = findViewById<Button>(R.id.criar_tweet)
+
+        botao.setOnClickListener { publicaTweet() }
+    }
+
+    private fun publicaTweet() {
+        Log.i("tweet", "botão clicado")
+
+        val campoDeMensagemDoTweet = findViewById<EditText>(R.id.tweet_mensagem)
+        val mensagemDoTweet : String = campoDeMensagemDoTweet.text.toString()
+        Toast.makeText(this, mensagemDoTweet, Toast.LENGTH_LONG).show()
     }
 }
