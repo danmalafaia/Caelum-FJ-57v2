3344fd44b19ff84e3d7fbd0fcdad1a5ea9da83af
Danilo Leite <danilo@moobie.com.br>
2019-03-16 15:18:11 -0300

    creating project

---
+++ app/src/main/java/br/com/caelum/twittelumapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.caelum.twittelumapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
